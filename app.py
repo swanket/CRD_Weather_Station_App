be8ba06a54c9e492107925de4fb898fc67a9b148
swanket <92056777+swanket@users.noreply.github.com>
2025-05-21 09:46:16 -0700

    adding url

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from st_supabase_connection import SupabaseConnection
 #     conn.close()
 #     return pl.DataFrame(rows, schema=cols)
 
-conn = st.connection("connections.supabase", type = SupabaseConnection)
+conn = st.connection("connections.supabase", url = "https://qlyjuhnwxvltrvwsptnv.supabase.co", type = SupabaseConnection)
 # st_supabase = st.connection(
 #     name="supabase_connection", 
 #     type=SupabaseConnection, 
