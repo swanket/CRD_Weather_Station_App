0c473b0d5493d84797691efc21d480428f5de400
swanket <92056777+swanket@users.noreply.github.com>
2025-05-21 09:07:17 -0700

    remove station name filter

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ st.title("Weather Station Explorer")
 
 # Example: Select station and number of rows
 # limit = st.slider("Number of rows", 10, 500, 100)
-station_filter = st.text_input("Filter by station name (optional)")
+# station_filter = st.text_input("Filter by station name (optional)")
 
 # rows = conn.query("*", table = "stations", ttl = "10m").execute()
 rows = conn.table("variables").select("*").execute()
