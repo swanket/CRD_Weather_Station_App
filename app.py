03c8fd576b4e2326198ec03e92c36b85d036e216
swanket <92056777+swanket@users.noreply.github.com>
2025-05-22 09:54:10 -0700

    test plotly map

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ st.title("Capital Region District Weather Station Explorer")
 
 st.map(pl.DataFrame(conn.table("stations").select("*").execute().data), latitude="Latitude",longitude="Longitude",size=100)
 
+fig = px.scatter_mapbox(pl.DataFrame(conn.table("stations").select("*").execute().data), lat="Latitude",lon="Longitude",text="Native ID")
+st.plotly_chart(fig)
+
 
 # rows = conn.query("*", table = "stations", ttl = "10m").execute()
 stations = conn.table("stations").select("*").execute() # .eq("Native ID","FW001")
@@ -35,11 +38,16 @@ if st.button("Show me a Table"):
         st.error("Enter a table name: stations, variables, readings, or station_readings")
 
 col1, col2 = st.columns(2)
+
+
 with col1:
     station_temp = st.text_input("Pick a Station (Native ID) to Plot Air Temperature")
     year = st.text_input('And a starting year')
     variable = st.selectbox('Which variable would you like to plot? (use variable_id)',('9','1','2','3','4','5','6','7','8'))
     # start_year = datetime(int(year),1,1)
+
+with col2:
+    st.checkbox('CHECKBOX???')
 
 if st.button('Plot'):
     if station_temp:
