8b81c36a5a03cff5e379bef0d29855cfb5f9417e
swanket <92056777+swanket@users.noreply.github.com>
2025-05-22 09:36:22 -0700

    map add size

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ conn = st.connection("supabase", type = SupabaseConnection)
 
 st.title("Capital Region District Weather Station Explorer")
 
-st.map(pl.DataFrame(conn.table("stations").select("*").execute().data), latitude="Latitude",longitude="Longitude")
+st.map(pl.DataFrame(conn.table("stations").select("*").execute().data), latitude="Latitude",longitude="Longitude",size=10)
 
 
 # rows = conn.query("*", table = "stations", ttl = "10m").execute()
