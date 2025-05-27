8e833cd4b55f92504ffd1545bb4b5ade39618191
swanket <92056777+swanket@users.noreply.github.com>
2025-05-26 19:57:08 -0700

    zoom 7

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ else:
     df_pd = df.to_pandas()
     layer = pdk.Layer("ScatterplotLayer",df_pd,get_position='[Longitude, Latitude]',get_color='[200, 30, 0, 160]',get_radius=500,pickable=True)
     # Set the viewport
-    view_state = pdk.ViewState(latitude=df_pd["Latitude"].mean(),longitude=df_pd["Longitude"].mean(),zoom=10,pitch=0)
+    view_state = pdk.ViewState(latitude=df_pd["Latitude"].mean(),longitude=df_pd["Longitude"].mean(),zoom=7,pitch=0)
     # st.write("ViewState Type:", type(view_state))
     # st.write("Data Type:", type(df))
     # st.write("Data Head:", df.head())
